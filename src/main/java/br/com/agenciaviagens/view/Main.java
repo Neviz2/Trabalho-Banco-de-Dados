e86047365445730d7a4699b89e104f5f6b94d9d6
Lucas Neves <lucasneves4004@gmail.com>
2025-10-09 22:33:56 +0000

    criado a main

--- src/main/java/br/com/agenciaviagens/view/Main.java
+++ src/main/java/br/com/agenciaviagens/view/Main.java
@@ -5,6 +5,7 @@ import br.com.agenciaviagens.model.Reserva;
 import br.com.agenciaviagens.repository.ReservaRepositorio;
 
 import java.util.List;
+import java.util.Scanner;
 
 
 
@@ -12,21 +13,56 @@ public class Main {
 
     public static void main(String[] args) {
         
-        // 1. Cria uma instância da CLASSE DE IMPLEMENTAÇÃO
+         Scanner tec = new Scanner(System.in);
+
+
         ReservaRepositorio meuRepositorio = new ReservaRepositorio();
+
+        int opcao = 0; 
+
+        do{
+            System.out.println("\n--- Menu de Opções ---");
+            System.out.println("1. Buscar todas as reservas");
+            System.out.println("2. Buscar reservas por cliente");
+            System.out.println("3. Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = tec.nextInt();
+            tec.nextLine(); // Consumir a quebra de linha
+
+            switch (opcao) {
+                case 1:
+                    // Lógica para buscar todas as reservas
+                    System.out.println("Buscando todas as reservas...");
+                    break;
+                case 2:
+                    // Lógica para buscar reservas por cliente
+                    System.out.print("Digite o nome do cliente: ");
+                    String nomeCliente = tec.nextLine();
+                    System.out.println("Buscando reservas para o cliente: " + nomeCliente);
+                    List<Reserva> buscarCliente = meuRepositorio.buscarCliente(nomeCliente);
+                    for (Reserva reserva : buscarCliente) {
+                        System.out.println(reserva);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Saindo...");
+                case 4:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+
+
+        }
+        while (opcao != 3);
+
         
-        
-        // 2. Chama o método e GUARDA o resultado em uma variável
-        //List<Reserva> todasAsReservas = meuRepositorio.buscarTodas();
+    
         
 
-        List<Reserva> buscarCliente = meuRepositorio.buscarCliente("Pedro Gomes");
-        
-        System.out.println("\n--- Buscando todas as reservas no banco de dados ---");
-        System.out.println("CLIENTE: " + buscarCliente);
 
-       // for (Reserva reserva : todasAsReservas) {
-       //     System.out.println(reserva);
-      //  }
+        tec.close();
+        
     }
 }
